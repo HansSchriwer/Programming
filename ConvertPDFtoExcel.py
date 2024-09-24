9dc83f0c3f680bdf0d20ed4202d5e48fa64fb1ff
Hans Schriwer <93672292+HansSchriwer@users.noreply.github.com>
2024-09-24 16:23:04 -0400

    Update ConvertPDFtoExcel.py
    3 methods converting pdf to excel

--- ConvertPDFtoExcel.py
+++ ConvertPDFtoExcel.py
@@ -39,19 +39,18 @@ print("PDF converted to Excel successfully!")
 # In[ ]:
 
 
-### For the tabula-py method:
+""" For the tabula-py method:
 You need to install tabula-py using pip install tabula-py
 Make sure you have Java installed on your system
-This method works well for PDFs with tabular data ###
+This method works well for PDFs with tabular data """
 
 
 # In[ ]:
 
-
-### For the pdftables_api method:
+""" For the pdftables_api method:
 You need to install the library using pip install git+https://github.com/pdftables/python-pdftables-api.git
 You need to sign up for an API key at PDFTables.com
-This method can handle a wider variety of PDF layouts ###
+This method can handle a wider variety of PDF layouts """
 
 
 # In[ ]:
@@ -120,7 +119,7 @@ pdf_to_excel(pdf_file, excel_file)
 # In[ ]:
 
 
-### This script does the following:
+""" This script does the following:
 It imports the necessary libraries: pdfplumber for PDF extraction and pandas for handling data and Excel output.
 The pdf_to_excel function takes two parameters: the path to the input PDF file and the desired path for the output Excel file.
 It opens the PDF using pdfplumber and iterates through all pages.
@@ -128,7 +127,7 @@ For each page, it attempts to extract tables using the extract_table() method.
 If tables are found, the data is collected in the data list.
 After processing all pages, if data was found, it creates a pandas DataFrame from the extracted data.
 The DataFrame is then saved as an Excel file using the to_excel() method.
-If no tables are found, it prints a message indicating so. ###
+If no tables are found, it prints a message indicating so. """
 
 
 # In[ ]:
@@ -140,8 +139,8 @@ If no tables are found, it prints a message indicating so. ###
 # In[ ]:
 
 
-### This method using pdfplumber has some advantages:
+""" This method using pdfplumber has some advantages:
 It can handle multiple pages and multiple tables within a PDF.
 It doesn't require Java installation, unlike the tabula-py method.
-It's a pure Python solution, which can be easier to set up and maintain.###
+It's a pure Python solution, which can be easier to set up and maintain. """
 
